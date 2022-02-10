437c6a95bbdc8420bb30dd0934d3c7fa8291e229
flyingsonu122 <flyingsonu736@gmail.com>
2022-02-10 19:55:58 +0530

    added a new file

---
+++ function.php
@@ -0,0 +1,14 @@
+<?php
+
+function sayHello(){  // without parameter
+    echo "Hello World!";
+}
+
+function sayHi($name){  // with parameter
+    echo "Hello $name";
+}
+
+// sayHello();
+sayHi("Sonu");
+
+?>
